3ed5a34e95923cd4a2cb114dcb12c1540ef534d3
DuyTruong159 <truongnhox159@gmail.com>
2024-01-06 17:40:47 +0700

    Setup Hello World, TailwindCss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-[#de302f]'>
-          Edit <code>src/App.js</code> and to 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className='text-[#de302f]'>Hello World!!</h1>
+    </>
   );
 }
 
